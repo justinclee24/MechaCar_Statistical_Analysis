c504b987a3e8b55cb987c2c79c7a81462b000363
justinclee24 <85330159+justinclee24@users.noreply.github.com>
2021-09-17 11:48:12 -0600

    add t-tests

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -5,3 +5,7 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F) #import Suspension Coil dataset
 total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
 lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create lot-grouped summary table with multiple columns
+t.test((suspension_coil_table$PSI),mu=1500) #compare sample versus population means
+t.test(subset(suspension_coil_table$PSI, suspension_coil_table$Manufacturing_Lot == "Lot1"),mu=1500) #compare sample versus population means
+t.test(subset(suspension_coil_table$PSI, suspension_coil_table$Manufacturing_Lot == "Lot2"),mu=1500) #compare sample versus population means
+t.test(subset(suspension_coil_table$PSI, suspension_coil_table$Manufacturing_Lot == "Lot3"),mu=1500) #compare sample versus population means
